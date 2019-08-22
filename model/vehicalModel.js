faf6cf3aba6afb2dc89c8178033d7b4d1b16373b
Harshad Kapei <kapeiharshad@gmail.com>
2019-08-22 20:09:34 +0530

    harshadCallback

--- model/vehicalModel.js
+++ model/vehicalModel.js
@@ -1,15 +1,15 @@
 var mongoose = require("mongoose");
 var vehical = require("../schema/vehical.js");
 module.exports = {
-  save: (data, callback) => {
+  save: (data, harshadCallback) => {
     var vehicalSave = new vehical(data)
     vehicalSave.save((err, data) => {
       if (err) {
         console.log("err", err)
-        callback(err, null)
+        harshadCallback(err, null)
       } else {
         console.log("data save")
-        callback(null, data)
+        harshadCallback(null, data)
       }
     })
   }
