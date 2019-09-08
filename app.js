c64495e53c72756c97ee05ee4ab7ebe31fa32fd7
Harshad Kapei <kapeiharshad@gmail.com>
2019-09-09 00:59:54 +0530

    env remaining

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const express = require("express");
 const app = express();
 const config = require("./config");
-var bodyParser = require("body-parser");
-var glob = require("glob");
+const bodyParser = require("body-parser");
+const glob = require("glob");
 // const port = 3000;
 let mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb://localhost:27017/expressProject",
-  {
-    useNewUrlParser: true
-  },
-  () => {
-    console.log("Mongodb Connected");
-  }
+    "mongodb://localhost:27017/expressProject", {
+        useNewUrlParser: true
+    },
+    () => {
+        console.log("Mongodb Connected");
+    }
 );
 let conn = mongoose.connection;
 app.use(bodyParser.json());
 
-glob("./controller/*.js", function(err, files) {
-  for (var i = 0; i <= files.length - 1; i++) {
-    var controller = require(`${files[i]}`);
-    var str1 = files[i].split("/");
-    var str2 = str1[2].split("Controller.js");
-    app.use(`/${str2[0]}`, controller);
-  }
+glob("./controller/*.js", function (err, files) {
+    for (var i = 0; i <= files.length - 1; i++) {
+        var controller = require(`${files[i]}`);
+        var str1 = files[i].split("/");
+        var str2 = str1[2].split("Controller.js");
+        app.use(`/${str2[0]}`, controller);
+    }
 });
-console.log(config.NODE_ENV);
+console.log("config:::::", process.env.portdev, process.env.portprod);
+console.log("NODE_ENV::::::", process.env.NODE_ENV)
 app.listen(config.port, () =>
-  console.log(`Example app listening on port ${config.port}!`)
+    console.log(`Example app listening on port ${config.port}!`)
 );
--- config.js
+++ config.js
@@ -1,8 +1,25 @@
 const dotenv = require("dotenv");
 dotenv.config();
-module.exports = {
-  endpoint: process.env.API_URL,
-  masterKey: process.env.API_KEY,
-  port: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV
+module.exports = function b() {
+  console.log("process.env.NODE_ENV", process.env.NODE_ENV)
+  switch (process.env.NODE_ENV) {
+    // case 'development':
+    //   return {
+    //     endpoint: process.env.API_URL,
+    //       masterKey: process.env.API_KEY,
+    //       port: process.env.PORT
+    //   };
+
+    // case 'production':
+    //   return {
+    //     port: process.env.PORT
+    //   };
+
+    // default:
+    //   return {
+    //     endpoint: process.env.API_URL,
+    //       masterKey: process.env.API_KEY,
+    //       // port: process.env.PORT_DEV
+    //   };
+  }
 };
